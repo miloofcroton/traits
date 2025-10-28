f1f28c22af38f43f04410d30f571e28d16d92add
Jack Toumey <jack@toumey.io>
2025-10-27 20:26:10 -0700

    additional function

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,20 @@ mod stack;
 
 use basket::Basket;
 use stack::Stack;
+use container::Container;
+
+fn add_string<T: Container<String>>(cont: &mut T, str: String) {
+  cont.put(str);
+}
 
 fn main() {
-  let b1 = Basket::new(String::from("testsetset"));
+  let mut b1 = Basket::new(String::from("testsetset"));
   let b2 = Basket::new(1);
   let b3 = Basket::new(true);
 
-  let s1 = Stack::new(vec![String::from("blah")]);
+  let mut s1 = Stack::new(vec![String::from("blah")]);
   let s2 = Stack::new(vec![1, 2, 3]);
+
+  add_string(&mut b1, String::from("hi"));
+  add_string(&mut s1, String::from("hi"));
 }

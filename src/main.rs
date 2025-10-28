b7412817b62e8caa292c9da8c0f685fb900a2a17
Jack Toumey <jack@toumey.io>
2025-10-27 20:13:58 -0700

    another model

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod basket;
+mod stack;
 
 use basket::Basket;
+use stack::Stack;
 
 fn main() {
   let b1 = Basket::new(String::from("testsetset"));
   let b2 = Basket::new(1);
   let b3 = Basket::new(true);
 
+  let s1 = Stack::new(vec![String::from("blah")]);
+  let s2 = Stack::new(vec![1, 2, 3]);
 }
